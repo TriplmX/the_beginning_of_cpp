14ecefeb779281c9d01b144794a767b8a2637b09
Muhammed <87816936+TriplmX@users.noreply.github.com>
2023-10-12 19:23:31 +0200

    Add files via upload

---
+++ VJEŽBA_11_10_23/VJEŽBA_11_10_23/VJEŽBA_11_10_23.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    //char znak = 'a';
+    //int integer = 2147483647;
+    ////Veličina integera je 4 byte-a (32 bita)
+    //int rezultat = integer + 1;
+    //cout << "Integer ima vrijednost: " << integer << ", dok ako saberemo s plus jedan:" << rezultat << endl;
+    //int datum_rodjenja;
+    //int trenutna_godina;
+    //cout << "Unijeti datum rodjenja: ";
+    //cin >> datum_rodjenja;
+    //cout << "Unijeti trenutnu godinu: ";
+    //cin >> trenutna_godina;
+    //cout << "Vas broj godina glasi: " << trenutna_godina - datum_rodjenja << endl;
+    //system("pause>0");
+    //cout << " " << endl;
+    //cout << " " << endl;
+    //cout << "HVALA NA TESTIRANJU OVOG DIJELA PROGRAMA, ZBOGOM. ";
+    //cout << " " << endl;
+    //cout << " " << endl;
+    //cout << " " << endl;
+    /*float broj1 = 4.684;
+    float broj2 = 2.0;
+    float rezultat = broj1 / broj2;
+    cout << rezultat << endl;
+    double broj3 = 4.684;
+    double broj4 = 2.0;
+    double rezultat1 = broj3 / broj4;
+    cout << rezultat1 << endl;*/
+
+    //ZADACI:
+    cout << "the magic is in the comments" << endl;
+
+    return 0;
+
+}
+
+//KORISNE PREČICE NA TASTATURI:
+// ctrl + b = BUILD
+// ctrl + k + c = PRETVORI SELEKTOVANI KOD U KOMENTAR
+// ctrl + k + u = ODKOMENTARIŠI SELEKTOVANI KOD
+// alt + shift + up = PIŠI NA VIŠE LINIJA KOD ODJEDNOM
+// alt + up/down = PREMJEŠTANJE LINIJE KODA NA DRUGO MJESTO
+
+//CHECKPOINT 1:12:40
